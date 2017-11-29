5aaf3b2169864fa3eac944c476e246ac7dc8a5ee
iVANWU0522 <wu0719900522@gmail.com>
2017-11-29 16:13:47 +1100

    paperclip set up

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :image, presence: true
+
+
+  has_attached_file :image, styles: { :medium => "640x", :thumb => "100x100" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
+
