6d0925f1df4bbf5c32b1d0e624f0e3fbcd2c5ae8
iVANWU0522 <wu0719900522@gmail.com>
2017-11-29 18:59:45 +1100

    post new and create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
 
   end
 
@@ -9,7 +10,9 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    redirect_to posts_path
   end
+
 
   private
 
