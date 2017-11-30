79d243535559c43e07c2216f73e5d268b8ab4665
iVANWU0522 <wu0719900522@gmail.com>
2017-11-30 18:56:44 +1100

    edit and delete post functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to :action => 'show', :id => @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to :action => 'index'
+  end
+
   private
 
   def post_params
