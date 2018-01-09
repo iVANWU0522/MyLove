7f5c52bb3656fc6d1d7484b6288e75b58244ebee
iVANWU0522 <wu0719900522@gmail.com>
2018-01-09 23:27:27 +1100

    comments controller create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,17 @@ class CommentsController < ApplicationController
 
 
   def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:success] = "You commented the hell out of that post!"
+      redirect_to :back
+    else
+      flash[:alert] = "Check the comment form, something went horribly wrong."
+      render root_path
+    end
+
 
   end
 
