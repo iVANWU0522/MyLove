06b893ec305ac35007c291f8c3a6e7440dc2ef77
iVANWU0522 <wu0719900522@gmail.com>
2018-01-13 08:49:57 +1100

    comments pagination ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
 
   def index
-    @comment = @post.comments.order('created_at asc')
+    @comments = @post.comments.order('created_at ASC')
 
     respond_to do |format|
       format.html { render layout: !request.xhr? }
