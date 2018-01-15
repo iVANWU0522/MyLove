0ce2be84f38b887c80af69d4b735e00730a115f4
iVANWU0522 <wu0719900522@gmail.com>
2018-01-15 18:10:57 +1100

    the minimum length of username change to 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
+  validates :user_name, presence: true, length: { minimum: 2, maximum: 16 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
