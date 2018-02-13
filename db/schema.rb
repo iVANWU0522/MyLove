53270bf1b5d9af8c6d8c8a09710a2331b50245ef
iVANWU0522 <wu0719900522@gmail.com>
2018-02-13 23:21:21 +1100

    like function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212075206) do
+ActiveRecord::Schema.define(version: 20180213122040) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
