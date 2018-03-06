bc9b916a168f28c4c6293d0579e496d29eb946f6
iVANWU0522 <wu0719900522@gmail.com>
2018-03-07 10:59:09 +1100

    notification unread display

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,5 @@ class Notification < ActiveRecord::Base
   belongs_to :notified_by, class_name: 'User'
   belongs_to :post
 
-  validates :user_id, :notified_by_id, :post_id, :notice_type, presence: true
+  validates :user_id, :notified_by_id, :post_id, :identifier, :notice_type, presence: true
 end
